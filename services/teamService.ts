933aa773a900a505b4d87cf9d015111c886061b4
yosefhaim707 <yosefhaimmalamud@gmail.com>
2024-09-30 18:28:13 +0300

    added validation to teams service

--- services/teamService.ts
+++ services/teamService.ts
@@ -15,9 +15,35 @@ export function addToTeam(players: Player[]): Team {
 }
 
 function isValid(players: Player[]): boolean {
-    let positions: object = {'PG': 0, 'SG': 0, 'SF': 0, 'PF': 0, 'C': 0};
+    let positions: number[] = [0, 0, 0, 0, 0];
+    let isDup: boolean = true;
     for (let index = 0; index < players.length; index++) {
-        const element = players[index];
-        
+        const element: Player = players[index];
+        switch (element.position) {
+            case 'PG':
+                positions[0] ++;
+                break;
+            case 'SG':
+                positions[1] ++;
+                break;
+            case 'SF':
+                positions[2] ++;
+                break;
+            case 'PF':
+                positions[3] ++;
+                break;
+            case 'C':
+                positions[4] ++;
+                break;
+            default:
+                break;
+        }
     }
+    for (let index = 0; index < positions.length; index++) {
+        const element = positions[index];
+        if (element > 1 || element < 1) {
+            isDup = false;
+        }
+    }
+    return isDup;
 }
