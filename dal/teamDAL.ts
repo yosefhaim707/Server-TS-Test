5021abafb6c332b2b3a9be3583f628ce0ba0afcf
yosefhaim707 <yosefhaimmalamud@gmail.com>
2024-09-30 17:50:27 +0300

    Added first api req plus started with the second

--- dal/teamDAL.ts
+++ dal/teamDAL.ts
@@ -1,11 +1,14 @@
 import { Team } from "../models/Team";;
 const teams = require("../data/teams.json");
-const FILE: string = "../data/teams.json";
+const FILE: string = "./data/teams.json";
 import * as fs from "fs";
 
-console.log(typeof(teams));
+
+
+console.log(teams);
 
 export function addTeam(team: Team): void {
     teams.push(team);
-    fs.writeFileSync(FILE, teams)
+    fs.writeFileSync(FILE, JSON.stringify(teams));
 }
+
